ce1603c793b8bca57542177f1097d73b777c4da0
Zenonia <theinhtooaung906@gmail.com>
2026-06-19 15:39:33 +0630

    Revert "feat(ar): add account filter to aged receivable"
    This reverts commit 8fe05de57004640e9e98ab89a53b38d873c6541e.

--- models/account_aged_receivable.py
+++ models/account_aged_receivable.py
@@ -6,25 +6,6 @@ from odoo import models
 class AccountAgedReceivableReportHandler(models.AbstractModel):
     _inherit = "account.aged.receivable.report.handler"
 
-    def _custom_options_initializer(self, report, options, previous_options):
-        super()._custom_options_initializer(report, options, previous_options=previous_options)
-
-        previous_options = previous_options or {}
-        previous_account_ids = previous_options.get("account_ids") or []
-        selected_account_ids = [int(account_id) for account_id in previous_account_ids]
-        selected_accounts = selected_account_ids and self.env["account.account"].with_context(active_test=False).search([
-            ("id", "in", selected_account_ids),
-            ("account_type", "=", "asset_receivable"),
-            *self.env["account.account"]._check_company_domain(report.get_report_company_ids(options)),
-        ]) or self.env["account.account"]
-
-        options["vhg_ar_account_filter"] = True
-        options["account_ids"] = selected_accounts.ids
-        options["selected_account_ids"] = selected_accounts.mapped("display_name")
-
-        if selected_accounts:
-            options["forced_domain"] = options.get("forced_domain", []) + [("account_id", "in", selected_accounts.ids)]
-
     def _aged_partner_report_custom_engine_common(self, options, internal_type, current_groupby, next_groupby, offset=0, limit=None):
         result = super()._aged_partner_report_custom_engine_common(
             options,
